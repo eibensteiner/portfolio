9b88132ec931ef83905fd8923139d968a72e5ebe
dowu <me@dowu.xyz>
2020-07-08 20:10:33 +0200

    Routes removed

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import experience from "../views/experience";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/experience",
-    name: "experience",
-    component: experience
-  }
+
 ];
 
 const router = new VueRouter({
--- src/views/experience.vue
+++
@@ -1,9 +0,0 @@
-<template> </template>
-
-<script>
-export default {
-  name: "ViewExperience"
-};
-</script>
-
-<style scoped></style>
