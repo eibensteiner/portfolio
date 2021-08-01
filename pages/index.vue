856549a27110538d83b06ca2fafaca510549d0f6
dowu <me@dowu.xyz>
2021-08-01 19:07:12 +0200

    Link styling for headings added

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,35 @@
 <template>
   <main>
-    <h1>I'm Dominik, a coding Designer from Austria, striving for visual clarity and simplicity.</h1>
+    <h1>
+      I'm Dominik, a coding Designer from Austria, striving for visual clarity
+      and simplicity.
+    </h1>
     <div>
       <div>
         <p>
-          Currently I’m working as a <strong>Product & Brand Designer</strong> at
-          <a href="https://blockpit.io" target="_blank"> Blockpit </a>, supporting thousands of Users with their crypto
-          taxes. Previously I worked as a <strong>UX Engineer</strong> at <a href="https://storyclash.com" target="_blank"> Storyclash </a>.
+          Currently I’m working as a
+          <strong>Product & Brand Designer</strong> at
+          <a href="https://blockpit.io" target="_blank">Blockpit</a>, supporting
+          thousands of Users with their crypto taxes. Previously I worked as a
+          <strong>UX Engineer</strong> at
+          <a href="https://storyclash.com" target="_blank">Storyclash</a>.
         </p>
         <p>
           If you want to follow my journey, you can do so on
-          <a href="https://twitter.com/wurmdo" target="_blank"> Twitter </a>, <a href="www.linkedin.com/in/dowu" target="_blank"> LinkedIn </a> or
-          <a href="https://github.com/dowu" target="_blank"> GitHub </a>
+          <a href="https://twitter.com/wurmdo" target="_blank"> Twitter</a>,
+          <a href="https://dribbble.com/dowu" target="_blank">Dribbble</a>,
+          <a href="www.linkedin.com/in/dowu" target="_blank">LinkedIn</a> or
+          <a href="https://github.com/dowu" target="_blank">GitHub</a>
         </p>
       </div>
 
       <div v-for="article of articles" :key="article.slug">
-        <nuxt-link :to="{ name: 'slug', params: { slug: article.slug } }">
-          <h2>
+        <h2>
+          <nuxt-link :to="{ name: 'slug', params: { slug: article.slug } }">
             {{ article.title }}
-            <code v-if="!article.completed" class="ml-2">WIP</code>
-          </h2>
-        </nuxt-link>
+          </nuxt-link>
+          <code v-if="!article.completed" class="ml-2">WIP</code>
+        </h2>
         <nuxt-content :document="article" />
       </div>
     </div>
