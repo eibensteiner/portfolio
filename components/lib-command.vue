0f91a890264bcae2eb78537a65c9cf8285cec658
dowu <me@dowu.xyz>
2021-08-06 23:07:08 +0200

    Constants excluded

--- components/lib-command.vue
+++ components/lib-command.vue
@@ -24,7 +24,7 @@
           ref="entry"
           @mousemove="setEntryActive(index)"
         >
-          {{ resultItem.item.title || resultItem.title }}
+          {{ resultItem.item.title }}
         </li>
       </ul>
     </div>
@@ -33,13 +33,25 @@
 
 <script>
 import Fuse from "fuse.js";
+import { commands } from "@/utils/constants";
 
 export default {
+  data() {
+    return {
+      fuse: null,
+      isActive: true,
+      search: "",
+      list: commands,
+      result: this.list,
+      counter: 0,
+    };
+  },
+
   mounted() {
-    var options = {
+    let options = {
       shouldSort: true,
+      threshold: 0.3,
       maxPatternLength: 32,
-      minMatchCharLength: 1,
       keys: ["title", "author.firstName"],
     };
     console.log(this.result);
@@ -62,179 +74,6 @@ export default {
     },
   },
 
-  data() {
-    return {
-      fuse: null,
-      isActive: true,
-      search: "",
-      list: [
-        {
-          title: "Old Man's War",
-          author: {
-            firstName: "John",
-            lastName: "Scalzi",
-          },
-        },
-        {
-          title: "The Lock Artist",
-          author: {
-            firstName: "Steve",
-            lastName: "Hamilton",
-          },
-        },
-        {
-          title: "HTML5",
-          author: {
-            firstName: "Remy",
-            lastName: "Sharp",
-          },
-        },
-        {
-          title: "Right Ho Jeeves",
-          author: {
-            firstName: "P.D",
-            lastName: "Woodhouse",
-          },
-        },
-        {
-          title: "The Code of the Wooster",
-          author: {
-            firstName: "P.D",
-            lastName: "Woodhouse",
-          },
-        },
-        {
-          title: "Thank You Jeeves",
-          author: {
-            firstName: "P.D",
-            lastName: "Woodhouse",
-          },
-        },
-        {
-          title: "The DaVinci Code",
-          author: {
-            firstName: "Dan",
-            lastName: "Brown",
-          },
-        },
-        {
-          title: "Angels & Demons",
-          author: {
-            firstName: "Dan",
-            lastName: "Brown",
-          },
-        },
-        {
-          title: "The Silmarillion",
-          author: {
-            firstName: "J.R.R",
-            lastName: "Tolkien",
-          },
-        },
-        {
-          title: "Syrup",
-          author: {
-            firstName: "Max",
-            lastName: "Barry",
-          },
-        },
-        {
-          title: "The Lost Symbol",
-          author: {
-            firstName: "Dan",
-            lastName: "Brown",
-          },
-        },
-        {
-          title: "The Book of Lies",
-          author: {
-            firstName: "Brad",
-            lastName: "Meltzer",
-          },
-        },
-        {
-          title: "Lamb",
-          author: {
-            firstName: "Christopher",
-            lastName: "Moore",
-          },
-        },
-        {
-          title: "Fool",
-          author: {
-            firstName: "Christopher",
-            lastName: "Moore",
-          },
-        },
-        {
-          title: "Incompetence",
-          author: {
-            firstName: "Rob",
-            lastName: "Grant",
-          },
-        },
-        {
-          title: "Fat",
-          author: {
-            firstName: "Rob",
-            lastName: "Grant",
-          },
-        },
-        {
-          title: "Colony",
-          author: {
-            firstName: "Rob",
-            lastName: "Grant",
-          },
-        },
-        {
-          title: "Backwards, Red Dwarf",
-          author: {
-            firstName: "Rob",
-            lastName: "Grant",
-          },
-        },
-        {
-          title: "The Grand Design",
-          author: {
-            firstName: "Stephen",
-            lastName: "Hawking",
-          },
-        },
-        {
-          title: "The Book of Samson",
-          author: {
-            firstName: "David",
-            lastName: "Maine",
-          },
-        },
-        {
-          title: "The Preservationist",
-          author: {
-            firstName: "David",
-            lastName: "Maine",
-          },
-        },
-        {
-          title: "Fallen",
-          author: {
-            firstName: "David",
-            lastName: "Maine",
-          },
-        },
-        {
-          title: "Monster 1959",
-          author: {
-            firstName: "David",
-            lastName: "Maine",
-          },
-        },
-      ],
-      result: this.list,
-      counter: 0,
-    };
-  },
-
   methods: {
     setActive() {
       this.isActive = true;
@@ -250,7 +89,7 @@ export default {
     focusOnSearch() {
       this.search = "";
       setTimeout(() => {
-        this.$refs.search.$el.focus(); // $el is needed to set focus on custom components
+        this.$refs.search.focus();
       }, 50);
     },
 
